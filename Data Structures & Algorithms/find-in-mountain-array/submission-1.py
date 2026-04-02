22d5ff2fea75e8f5bcb4eb37a21bc0f64c96fcd1
John <97593853+hylinny@users.noreply.github.com>
2026-04-02 19:12:44 +0800

    Add: find-in-mountain-array - submission-1

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-1.py
@@ -0,0 +1,44 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        # increasing then decreasing array
+        # target can either be left or right side or none. if both, return left side
+        # first binary search finds peak (peak finding, elements strict)
+        # second one searches both sides
+        n = mountainArr.length()
+        hashmap = {} # cache mountainArr.get calls
+        def get(i):
+            if i not in hashmap:
+                hashmap[i] = mountainArr.get(i)
+            return hashmap[i]
+            
+        l, r = 0, n - 1
+        while l < r:
+            m = (r - l) // 2 + l
+            if get(m) < get(m+1):
+                l = m + 1
+            else:
+                r = m
+        # l == r, points to peak 
+        # search left side first
+        peak = l
+        l, r = 0, peak
+        while l <= r:
+            m = (r - l) // 2 + l
+            val = get(m)
+            if val == target:
+                return m
+            elif val < target:
+                l = m + 1
+            else:
+                r = m - 1
+        l, r = peak + 1, n-1
+        while l <= r:
+            m = (r - l) // 2 + l
+            val = get(m)
+            if val == target:
+                return m
+            elif val < target:
+                r = m - 1
+            else:
+                l = m + 1
+        return -1
