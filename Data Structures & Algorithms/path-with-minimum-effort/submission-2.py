d3a21fb6615f0143960505bfe2f72d97bdf72471
John <97593853+hylinny@users.noreply.github.com>
2026-07-04 16:27:51 +0800

    Add: path-with-minimum-effort - submission-2

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        heap = []
+        # change relaxation condition: take the max(edge weight, weight to current node
+        row = len(heights)
+        col = len(heights[0])
+        heapq.heappush(heap, (0, 0, 0))
+        minPath = {}
+        minPath[(0, 0)] = 0
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while heap:
+            weight, r, c = heapq.heappop(heap)
+            if r == row-1 and c == col-1:
+                return weight
+            if weight > minPath[(r, c)]:
+                continue
+            # relax neighbours
+            for dy, dx in directions:
+                newrow, newcol = dy + r, dx + c
+                if 0 <= newrow < row and 0 <= newcol < col:
+                    newEffort = max(abs(heights[newrow][newcol] - heights[r][c]), weight)
+                    if (newrow, newcol) not in minPath or minPath[(newrow, newcol)] > newEffort:
+                        heapq.heappush(heap, (newEffort, newrow, newcol))
+                        minPath[(newrow, newcol)] = newEffort
+
+        return minPath[(row-1, col-1)]
+
