2d8895c383a7bc324b7bb1b3d31b7b0a6becf224
John <97593853+hylinny@users.noreply.github.com>
2026-07-25 19:26:35 +0800

    Add: roman-to-integer - submission-1

---
+++ Data Structures & Algorithms/roman-to-integer/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        hashmap = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            "IV": 4,
+            "IX": 9,
+            "XL": 40,
+            "XC": 90,
+            "CD": 400,
+            "CM": 900
+        }
+        integer = 0
+        i = 0
+        while i < len(s):
+            if i < len(s)-1 and s[i:i+2] in hashmap:
+                integer += hashmap[s[i:i+2]]
+                i += 2
+            else:
+                integer += hashmap[s[i]]
+                i += 1
+        return integer
